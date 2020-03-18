40e1be79ea0f22f1d48c97a8d5828499da596bdc
kws970609 <kws970609@users.noreply.github.com>
2020-03-18 11:43:43 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200318_03_listview01/datas/Store.java
@@ -0,0 +1,27 @@
+package kr.co.tjoeun.a20200318_03_listview01.datas;
+
+import java.io.Serializable;
+
+public class Store implements Serializable {
+
+    private  String name;
+    private int rating; // 별을 몇개 찍어줘야 하는지.(평점)
+
+//    Alt + Insert => 생성자 / getter ,setter 만들기
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
